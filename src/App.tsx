cad26f11c45ea9337781e5ed065a83e26ca1c06b
Domagoj <paulic.domagoj@gmail.com>
2023-12-30 13:19:32 +0100

    fetch and display data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
+import { useQuery } from 'convex/react';
+import { api } from '../convex/_generated/api';
 
 function App() {
+  const tasks = useQuery(api.tasks.get);
   return (
     <>
       <h1>Convex task test</h1>
-      <div className="card"></div>
+      <div className="card">
+        {tasks?.map(({ _id, text }) => (
+          <div key={_id}>{text}</div>
+        ))}
+      </div>
     </>
   );
 }
